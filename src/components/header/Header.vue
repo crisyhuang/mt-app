f4ab70cc85caf17fceb015fb47801610476e7288
黄佳佳 <huangjiajia@huangjiajiadeMacBook-Pro.local>
2018-07-03 14:52:11 +0800

    Header组件: 增加活动弹窗+纯css3实现关闭按钮

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -21,9 +21,7 @@
 
     <!-- 商家信息 start -->
     <div class="business-info">
-      <div class="pic">
-        <img :src="poiInfo.pic_url" alt="">
-      </div>
+      <img class="pic" :src="poiInfo.pic_url" alt="">
       <div class="name">{{poiInfo.name}}</div>
       <div class="favor">收藏</div>
     </div>
@@ -32,21 +30,48 @@
 
     <!-- 优惠活动 start -->
     <div class="discount-activity">
-      <img class="icon" v-if="poiInfo.discounts2" :src="poiInfo.discounts2[0].icon_url">
+      <img class="pic" v-if="poiInfo.discounts2" :src="poiInfo.discounts2[0].icon_url">
       <span class="text" v-if="poiInfo.discounts2">{{poiInfo.discounts2[0].info}}</span>
-      <a class="activity-icon" v-if="poiInfo.discounts2">
+      <a class="activity-icon" v-if="poiInfo.discounts2" @click="showMask">
         {{poiInfo.discounts2.length}}个活动
         <span class="icon-keyboard_arrow_right"></span>
       </a>
     </div>
     <!-- 优惠活动 end -->
+
+    <!-- 优惠活动弹窗 start -->
+    <div class="activity-mask" v-show="isShowMask">
+      <div class="wrapper">
+        <div class="content" :style="mask_bg_url">
+          <img class="pic" :src="poiInfo.pic_url">
+          <h3 class="name">{{poiInfo.name}}</h3>
+          <div class="score">
+            <span>{{poiInfo.wm_poi_score}}</span>
+          </div>
+          <p class="tips">
+            {{poiInfo.min_price_tip}} <i>|</i> {{poiInfo.shipping_fee_tip}} <i>|</i> {{poiInfo.delivery_time_tip}}
+          </p>
+          <p class="time">配送时间：{{poiInfo.shipping_time}}</p>
+          <div class="discounts">
+            <p>
+              <img v-if="poiInfo.discounts2" :src="poiInfo.discounts2[0].icon_url">
+              <span v-if="poiInfo.discounts2">{{poiInfo.discounts2[0].info}}</span>
+            </p>
+          </div>
+        </div>
+        <a href="javascript:;" class="u-close" @click="closeMask"></a>
+      </div>
+    </div>
+    <!-- 优惠活动弹窗 end -->
   </div>
 </template>
 
 <script>
   export default {
     data(){
-      return {}
+      return {
+        isShowMask: false
+      }
     },
     props: {
       poiInfo: {
@@ -57,6 +82,17 @@
     computed: {
       header_bg_url(){
         return "background-image: url(" + this.poiInfo.head_pic_url + ")"
+      },
+      mask_bg_url(){
+        return "background-image: url(" + this.poiInfo.poi_back_pic_url + ")"
+      }
+    },
+    methods:{
+      showMask(){
+        this.isShowMask = true
+      },
+      closeMask(){
+        this.isShowMask = false
       }
     }
   }
@@ -83,7 +119,6 @@
   .top-nav .back-btn{
     width: 1rem;
     text-align: center;
-    color: #fff;
   }
   .top-nav .search-box{
     flex: 1;
@@ -143,10 +178,6 @@
     width: 1rem;
     height: 1rem;
     margin-right: 0.15rem;
-  }
-  .business-info .pic img{
-    width: 100%;
-    height: 100%;
     border-radius: 5px;
   }
   .business-info .name{
@@ -174,7 +205,7 @@
     align-items: center;
     margin-top: 0.1rem;
   }
-  .discount-activity img{
+  .discount-activity .pic{
     width: 0.3rem;
     height: 0.3rem;
     margin-right: 0.1rem;
@@ -189,5 +220,90 @@
     text-align: right;
     color: #fff;
   }
+  .activity-mask{
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 99;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+  }
+  .activity-mask h3, .activity-mask p, .activity-mask span{
+    color: #fff;
+  }
+  .activity-mask .wrapper{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+  .activity-mask .content{
+    width: 6.5rem;
+    height: 12rem;
+    background-size: 7rem 12rem;
+    border-radius: 10px;
+  }
+  .activity-mask .content .pic{
+    width: 1.5rem;
+    height: 1.5rem;
+    margin-top: 0.8rem;
+    border-radius: 5px;
+  }
+  .activity-mask .content h3{
+    margin: 0.25rem 0 0.1rem;
+    font-size: 14px;
+    font-weight: normal;
+  }
+  .activity-mask .content p{
+    line-height: 2;
+    color: #bababc;
+  }
+  .activity-mask .content .tips i{
+    margin: 0 0.14rem;
+    font-style: normal;
+    color: #bababc;
+  }
+  .activity-mask .content .discounts{
+    width: 90%;
+    margin: 0.2rem auto;
+    padding-top: 0.2rem;
+    border-top: 1px solid #bababc;
+  }
+  .activity-mask .content .discounts img{
+    width: 0.3rem;
+    height: 0.3rem;
+    margin-right: 0.1rem;
+    vertical-align: text-bottom;
+    border-radius: 2px;
+  }
+  .activity-mask .u-close{
+    position: relative;
+    display: inline-block;
+    width: 0.8rem;
+    height: 0.8rem;
+    margin-top: 0.3rem;
+    background: rgba(118, 118, 118, 0.7);
+    border-radius: 50%;
+    transform: rotate(45deg);
+  }
+  .activity-mask .u-close:before, .activity-mask .u-close:after{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    content: '';
+    width: 1px;
+    height: 15px;
+    background: #fff;
+  }
+  .activity-mask .u-close:before{
+    transform: translate(-50%, -50%) rotate(90deg);
+    transform-origin: 50% 50%;
+  }
+
+
+
 
 </style>
